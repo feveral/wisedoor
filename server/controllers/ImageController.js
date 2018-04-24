197d1423b87bbf9cb838fcb5a93ff537b467668a
feveral <feveraly@gmail.com>
2018-04-25 02:17:22 +0800

    temp

--- server/controllers/ImageController.js
+++ server/controllers/ImageController.js
@@ -4,11 +4,12 @@ let count = 0
 
 module.exports = { 
     upload (req,res) {
+        
         fs.writeFile(`./out${count}.png`, req.body.image, 'base64',  err => {
             count ++ 
             if (err) res.status(500),send({
                 error: 'an error has occured trying to upload image'
-            });
+            })
             else res.send({
                 success: 'upload image succeed'
             })
