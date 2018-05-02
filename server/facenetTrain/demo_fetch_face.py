a5f027e7b64e77078e8c09863449bd6d52202218
john0829 <joh860829nny@gmail.com>
2018-05-02 12:16:20 +0800

    demo file test

--- server/facenetTrain/demo_fetch_face.py
+++ server/facenetTrain/demo_fetch_face.py
@@ -4,15 +4,22 @@ from Train import *
 from Camera import Camera
 import time
 import os
+import shutil
+
 
 dirName = raw_input("what is your name:")
 rawFolder = './image/raw/demo/'
 cutFolder = './image/cut/demo/'
 nowDirectory = sys.path[0] 
-print(nowDirectory)
+if not os.path.exists(rawFolder):
+    os.makedirs(rawFolder)
+if not os.path.exists(cutFolder):
+    os.makedirs(cutFolder)
 if not os.path.exists('./image/raw/demo/' +  dirName):
     os.makedirs('./image/raw/demo/' + dirName)
-
+if os.path.exists('./image/cut/demo/' +  dirName):
+    shutil.rmtree('./image/cut/demo/' +  dirName)
+    
 camera = Camera()
 for i in range(25):
     imageFrame = camera.CatchImage()
