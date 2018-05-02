36c200b458619df717605961b95388e542b50adb
feveral <feveraly@gmail.com>
2018-05-02 11:53:47 +0800

    temp

--- server/facenetTrain/demo.py
+++ server/facenetTrain/demo.py
@@ -10,5 +10,5 @@ camera = Camera()
 for i in range(25):
     imageFrame = camera.CatchImage()
     imagePath = rawFolder + '/' + str(i) + '.jpg'
-    camera.SaveImage( imagePath, imageFrame)
+    camera.SaveImage(imagePath, imageFrame)
 CutPicture(rawFolder,cutFolder)
