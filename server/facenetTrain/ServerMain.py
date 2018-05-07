271731e03dc37921b8dd8b0542786a60db037a1f
john0829 <joh860829nny@gmail.com>
2018-05-07 19:07:26 +0800

    temp

--- server/facenetTrain/ServerMain.py
+++ server/facenetTrain/ServerMain.py
@@ -1,9 +1,9 @@
 from FacenetAlignSingle import *
 from Train import *
-cutPicture = CutPicture('./image/raw/test/tom/517ec57bf6bbdadd3456eafaa256ae.png', "./image/cut/test","align")
+cutPicture = CutPicture('./image/raw/test/tom/517ec57bf6bbdadd3456eafaa256ae.png', "./image/cut/test", "align")
 
 train = Train("./facenetTrain/image/cut","./facenetTrain/models/tom2.pkl")
 
-import requests
-reply = requests.get("http://localhost/api/run/modelFinish")
-print(reply.content)
+# import requests
+# reply = requests.get("http://localhost/api/run/modelFinish")
+# print(reply.content)
