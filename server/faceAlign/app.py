dbe486e33c98093b4f3371828075dc9c1cc86ba3
feveral <feveraly@gmail.com>
2018-05-10 00:11:26 +0800

    update gitignore

--- server/faceAlign/app.py
+++ server/faceAlign/app.py
@@ -23,4 +23,4 @@ def alignPicture():
     return jsonify({'success': data})
 
 if __name__ == '__main__':
-    app.run(host='localhost', debug=True, port = 3000)
+    app.run(host='localhost', debug=True, port=3000)
