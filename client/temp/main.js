a2038bd951f82aa8987cf83c29cb7c4f68356a69
feveral <feveraly@gmail.com>
2018-08-23 21:03:32 +0800

    test Camera

--- client/temp/main.js
+++ client/temp/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Put variables in global scope to make them available to the browser console.
-const constraints = window.constraints = {
+const constraints = /*window.constraints = */{
   audio: false,
   video: true
 };
